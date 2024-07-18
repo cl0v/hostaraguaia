4f0e3123eab51c42a46e100e29c728aa07f0135c
Marcelo Viana <marcelofv12@hotmail.com>
2024-07-18 05:44:35 -0300

    add default dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 import 'src/home/ui/home_page.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -15,12 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hostaraguaia',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blueAccent,
-        ),
-        useMaterial3: true,
-      ),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      theme: ThemeData.light(useMaterial3: true),
       home: const HomePage(),
     );
   }
