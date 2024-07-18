2a7099262408a7e75ffe42d19b3c93bd32746a51
Marcelo Viana <marcelofv12@hotmail.com>
2024-07-18 10:02:20 -0300

    fix: removed blank images

--- lib/src/home/data/datasources/watchmode_datasource.dart
+++ lib/src/home/data/datasources/watchmode_datasource.dart
@@ -1,8 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:hostaraguaia/src/home/data/models/movie_model.dart';
+import 'package:hostaraguaia/src/home/domain/entities/movie_card_entity.dart';
 import 'package:hostaraguaia/src/http/keys.dart';
 
 abstract class IMoviesSearchDataSource {
-  Future<List<dynamic>> searchMovies(String query);
+  Future<List<MovieCardEntity>> searchMovies(String query);
 }
 
 class WatchModeDataSourceImpl implements IMoviesSearchDataSource {
@@ -11,8 +13,40 @@ class WatchModeDataSourceImpl implements IMoviesSearchDataSource {
   const WatchModeDataSourceImpl(this.client);
 
   @override
-  Future<List> searchMovies(String query) async {
-    final r = await client.get('https://api.watchmode.com/v1/releases/?apiKey=$watchmode_api_key');
-    return r.data;
+  Future<List<MovieCardEntity>> searchMovies(String query) async {
+    if (query.isEmpty) {
+      return _listReleases();
+    }
+    final url = Uri.parse(
+      'https://api.watchmode.com/v1/autocomplete-search/?apiKey=$watchmode_api_key&search_value=$query&search_type=3',
+    ).toString();
+    final r = await client.get(url);
+    List<MovieCardEntity> movies = [];
+    for (var item in r.data['results']) {
+      if (item['image_url'] == null ||
+          item['image_url'] == '' ||
+          item['image_url'] == 'https://cdn.watchmode.com/posters/blank.gif') {
+        continue;
+      }
+      movies.add(MovieCardModel.fromJson(item));
+    }
+    return movies;
+    // curl -i 'https://api.watchmode.com/v1/autocomplete-search/?apiKey=$watchmode_api_key&search_value=$query&search_type=3'
+  }
+
+  Future<List<MovieCardEntity>> _listReleases() async {
+    final r = await client.get(
+      'https://api.watchmode.com/v1/releases/?apiKey=$watchmode_api_key',
+    );
+    List<MovieCardEntity> movies = [];
+    for (var item in r.data['releases']) {
+      if (item['poster_url'] == null ||
+          item['poster_url'] == '' ||
+          item['poster_url'] == 'https://cdn.watchmode.com/posters/blank.gif') {
+        continue;
+      }
+      movies.add(MovieCardReleasesModel.fromJson(item));
+    }
+    return movies;
   }
 }
