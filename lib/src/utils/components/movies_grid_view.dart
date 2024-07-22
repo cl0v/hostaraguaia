d0cc1ca90bd25b6c34d12ea60e8165b5074ce8b9
Marcelo Viana <marcelofv12@hotmail.com>
2024-07-22 17:45:02 -0300

    add logic

--- lib/src/utils/components/movies_grid_view.dart
+++ lib/src/utils/components/movies_grid_view.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hostaraguaia/src/home/ui/blocs/search_bloc.dart';
 import 'package:hostaraguaia/src/utils/computed/grid_cards_count.dart';
 
 import '../../home/domain/entities/movie_card_entity.dart';
 import 'movie_card.dart';
 
-class MoviesGridViewComponent extends StatelessWidget {
+class MoviesGridViewComponent extends StatefulWidget {
   const MoviesGridViewComponent({
     super.key,
     required this.movies,
   });
 
   final List<MovieCardEntity> movies;
+
+  @override
+  State<MoviesGridViewComponent> createState() =>
+      _MoviesGridViewComponentState();
+}
+
+class _MoviesGridViewComponentState extends State<MoviesGridViewComponent> {
+  final _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,10 +42,29 @@ class MoviesGridViewComponent extends StatelessWidget {
           crossAxisSpacing: 4,
         ),
         itemBuilder: (context, index) => MovieCardComponent(
-          movie: movies[index],
+          movie: widget.movies[index],
         ),
-        itemCount: movies.length,
+        itemCount: widget.movies.length,
       );
     });
+  }
+
+  @override
+  void dispose() {
+    _scrollController
+      ..removeListener(_onScroll)
+      ..dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_isBottom) context.read<SearchBloc>().add('');
+  }
+
+  bool get _isBottom {
+    if (!_scrollController.hasClients) return false;
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.offset;
+    return currentScroll >= (maxScroll * 0.9);
   }
 }
